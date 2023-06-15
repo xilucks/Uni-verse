43a13eeddedc6ba275ce7a4e1c4184f2c16e14d6
Choi siun <siun0331@gmail.com>
2023-06-15 19:48:51 +0900

    feat: Post Card Link add

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,25 +1,33 @@
 import { allPosts } from "contentlayer/generated";
-import PostCard, {PostCardProps} from "@/components/PostCard/organisms/PostCard";
-export function getSortedPostList(){
-    const postList: any[] = allPosts.sort((prevPost, nextPost) => Number(new Date(nextPost.date)) - Number(new Date(prevPost.date)));
+import PostCard, {
+  PostCardProps,
+} from "@/components/PostCard/organisms/PostCard";
+export function getSortedPostList() {
+  const postList: any[] = allPosts.sort(
+    (prevPost, nextPost) =>
+      Number(new Date(nextPost.date)) - Number(new Date(prevPost.date))
+  );
 
-    return postList;
+  return postList;
 }
 export default function PageHome() {
-    const postList = getSortedPostList();
-    return (
-        <div className={'text-center w-full'}>
-            {postList.map((post: PostCardProps) => {
-                return <PostCard
-                            key={`${post.title}${post.createTime}`}
-                            title={post.title}
-                            description={post.description}
-                            imgSrc={post.imgSrc}
-                            imgAlt={post.imgAlt}
-                            tags={post.tags}
-                            createTime={post.createTime} />
-            })}
-
-        </div>
-    )
+  const postList = getSortedPostList();
+  return (
+    <div className={"text-center w-full"}>
+      {postList.map((post: PostCardProps) => {
+        return (
+          <PostCard
+            key={post._id}
+            title={post.title}
+            description={post.description}
+            imgSrc={post.imgSrc}
+            imgAlt={post.imgAlt}
+            tags={post.tags}
+            createTime={post.createTime}
+            slug={post._raw.flattenedPath}
+          />
+        );
+      })}
+    </div>
+  );
 }
--- src/components/PostCard/organisms/PostCard.tsx
+++ src/components/PostCard/organisms/PostCard.tsx
@@ -1,27 +1,44 @@
-'use client'
+"use client";
 
 import dayjs from "dayjs";
 import TimeMolecules from "@/components/PostCard/molecules/TimeMolecules";
 import ContentSummary from "@/components/PostCard/molecules/ContentSummary";
 import ImageContainer from "@/components/PostCard/atom/ImageContainer";
+import Link from "next/link";
 
 export interface PostCardProps {
-    title: string,
-    description: string,
-    imgSrc: string,
-    imgAlt: string,
-    tags: string[],
-    createTime: dayjs.ConfigType,
+  title: string;
+  description: string;
+  imgSrc: string;
+  imgAlt: string;
+  tags: string[];
+  createTime: dayjs.ConfigType;
+  slug: any;
 }
 
-export default function PostCard(props: PostCardProps){
-    return(
-        <article className={'card w-72 max-w-[280px] h-80 bg-base-100 text-left hover:shadow-lg dark:hover:shadow-black/30 hover:scale-105 mx-auto sm:m-0'}>
-            <ImageContainer imgSrc={props.imgSrc} imgAlt={props.imgAlt} width={400} height={200} />
-            <div className={'card-body h-3/6 pb-2 px-4 pt-4'}>
-                <ContentSummary title={props.title} description={props.description} tags={props.tags} />
-                <TimeMolecules createTime={props.createTime} />
-            </div>
-        </article>
-    )
+export default function PostCard(props: PostCardProps) {
+  return (
+    <Link href={`/post/${props.slug}`} passHref>
+      <article
+        className={
+          "card w-72 max-w-[280px] h-80 bg-base-100 text-left hover:shadow-lg dark:hover:shadow-black/30 hover:scale-105 mx-auto sm:m-0"
+        }
+      >
+        <ImageContainer
+          imgSrc={props.imgSrc}
+          imgAlt={props.imgAlt}
+          width={400}
+          height={200}
+        />
+        <div className={"card-body h-3/6 pb-2 px-4 pt-4"}>
+          <ContentSummary
+            title={props.title}
+            description={props.description}
+            tags={props.tags}
+          />
+          <TimeMolecules createTime={props.createTime} />
+        </div>
+      </article>
+    </Link>
+  );
 }
