8977f773069ff3158567853d42f48b4f60a218a9
Choi siun <siun0331@gmail.com>
2023-06-23 15:06:39 +0900

    feat: mdx 파일로 가져오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main>
       <div>홈페이지입니다.</div>
     </main>
-  )
+  );
 }
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -13,7 +13,7 @@ export function getSortedPostList() {
 export default function PageHome() {
   const postList = getSortedPostList();
   return (
-    <div className={"text-center w-full h-screen"}>
+    <div className={"text-center w-full"}>
       {postList.map((post: PostCardProps) => {
         return (
           <PostCard
